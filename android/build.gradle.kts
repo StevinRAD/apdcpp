14657ff0e5deddc69416a21b2f99912f38a188fa
StevinRAD <sstepin3rd@gmail.com>
2026-05-14 21:18:30 +0700

    Fix: Pindahkan deklarasi plugin Firebase ke settings.gradle.kts (cara modern)

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,9 +1,3 @@
-plugins {
-    id("com.google.gms.google-services") version "4.4.2" apply false
-    id("com.google.firebase.crashlytics") version "3.0.2" apply false
-    id("com.google.firebase.perf") version "1.4.2" apply false
-}
-
 buildscript {
     repositories {
         google()
